da29809ea99c5131ea5673a085db71c4be15b4e7
Andrew <vierhein@gmail.com>
2020-04-14 13:18:07 +0300

    init project

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex>
+     
+      <v-btn @click="message">
+        new message
+      </v-btn>
+
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  sockets: {
+    connect: function () {
+        console.log('socket connected')
+    }
+  },
+  methods: {
+    message() {
+      this.$socket.emit("createMessage", {
+        text: "FROM CLIENT"
+      })
+    }
+  }
+}
+</script>
---
+++ server/app.js
@@ -0,0 +1,22 @@
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+io.on('connection', socket => {
+
+    console.log('io connected')
+    
+    socket.on('createMessage', data => {
+        setTimeout(() => {
+            socket.emit('newMessage', {
+                text: data.text + ' SERVER'
+            })
+        }, 500)
+    })
+
+})
+
+module.exports = {
+    app, 
+    server
+}
