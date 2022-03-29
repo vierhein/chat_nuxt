127a22d93fa4a59f1058aeaa159657616c41122f
Light <vierhein@gmail.com>
2022-03-29 19:24:13 +0300

    add backend admin messages

--- server/app.js
+++ server/app.js
@@ -1,10 +1,24 @@
+const { join } = require('path')
+
 const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
+const m = (name, text, id) => ({name, text, id})
+
 io.on('connection', socket => {
 
-    console.log('io connected')
+    socket.on('userJoined', (data, cb) => {
+        if (!data.name || !data.room) {
+            return cb('invalid data')
+        }
+        socket.join(data.room)
+        cb({userId: socket.id})
+        socket.emit('newMessage', m('admin', `Hi, ${data.name}`))
+        socket.broadcast
+            .to(data.room)
+            .emit('newMessage', m('admin', `${data.name} joined.`))
+    })
     
     socket.on('createMessage', data => {
         setTimeout(() => {
